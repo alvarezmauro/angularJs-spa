40c4bdaf2d2713ee7dbc9c8bb366352227243adf
Mauro Alvarez <mauroalvarez86@gmail.com>
2015-10-19 21:44:55 +1100

    Disable Karma + use variable interpolation to add animations

--- Gruntfile.js
+++ Gruntfile.js
@@ -541,7 +541,9 @@ module.exports = function ( grunt ) {
    * before watching for changes.
    */
   grunt.renameTask( 'watch', 'delta' );
-  grunt.registerTask( 'watch', [ 'build', 'karma:unit', 'delta' ] );
+  grunt.registerTask( 'watch', [ 'build', 'delta' ] );
+
+  //'karma:unit',
 
   /**
    * The default task is to build and compile.
@@ -554,9 +556,11 @@ module.exports = function ( grunt ) {
   grunt.registerTask( 'build', [
     'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'less:build',
     'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
-    'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
-    'karma:continuous' 
+    'copy:build_appjs', 'copy:build_vendorjs', 'index:build'
   ]);
+  //
+  //'karmaconfig',
+  //  'karma:continuous'
 
   /**
    * The `compile` task gets your app ready for deployment by concatenating and
